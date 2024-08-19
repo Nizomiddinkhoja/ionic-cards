a486003c25d2c3ca3b86c3ab3e3e14e0c17165cc
nizomiddinkhoja <nizomiddinkhoja@gmail.com>
2024-08-19 15:15:48 +0300

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'cards',
+    loadChildren: () => import('./pages/cards/cards.module').then(m => m.CardsModule)
+  },
+  {
+    path: 'todos',
+    loadChildren: () => import('./pages/todos/todos.module').then(m => m.TodosModule)
   },
   {
     path: '',
@@ -15,7 +23,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
